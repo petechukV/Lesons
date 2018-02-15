6a40b2fe1845f9dd7590f20e9ef3ebd2d54975d0
Vlad <petechukvlad@gmail.com>
2018-02-15 23:17:22 +0200

    10

---
+++ script2.js
@@ -0,0 +1,65 @@
+var i = 999999999999999;
+var j = 9999999999999999;
+
+console.log(i);
+console.log(j);
+
+console.log(0xff.toString(2));
+
+console.log( (0.1 * 10 + 0.2 * 10) / 10 );
+console.log( 10 / 0 );
+console.log( -10 / 0 );
+console.log( -10 / "tr" );
+console.log(isNaN( (-10 / "tr")+"11" ));
+
+var t=1.458;
+var s = parseInt('11dogs',2);
+console.log(s);
+
+var d = parseFloat("1.2548dogs");
+console.log(d);
+
+console.log(t.toFixed(10));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
